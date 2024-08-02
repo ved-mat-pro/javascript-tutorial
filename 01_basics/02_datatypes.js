7590804f7410a52690f80864a3181971715e9f7b
ved-mat-pro <131252778+ved-mat-pro@users.noreply.github.com>
2024-08-02 11:59:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all Js code as newer version
+
+//alert(3+3)    alert will not work directly in node.js but it will work in console pannel of web
+
+console.log(3+3)
+console.log('vedant')
+
+let name = 'vedant'
+let age = 20
+let isLoggedIn = false
+
+
+// number = 2 to power 53 
+// bigInt
+// string => '' or ""
+// boolean => true/false
+// null =>stand alone value
+// undefined
+// symbol
+
+//null is an object datatype
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
