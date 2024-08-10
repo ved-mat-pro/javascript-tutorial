687af136749a038ae60f100a67c6b60f916c1b3f
ved-mat-pro <131252778+ved-mat-pro@users.noreply.github.com>
2024-08-10 18:36:57 +0000

    #

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -17,3 +17,36 @@ let somenumber = 33
 let stringnumber = String(somenumber)
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+// *******************Operations**************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " vedant"
+let str3 = str1 + str2
+console.log(str3);
+let str4 = str1 - str2
+console.log(str4);      //output is NaN
+
+console.log("1" + 2);       //output is 12
+console.log(1 + "2");       //output is 12
+console.log("1" + 2 + 2);       //output is 122
+console.log(1 + 2 + "2");       //output is 32
+
+console.log(+true);     //output is 1
+console.log(+"");       //output is 0
+
+num1 = num2 = num3 = 2+2
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);       //output is 101
+
+
+
