5aba3446cc5f080475db33d67884fbd92c390685
ved-mat-pro <131252778+ved-mat-pro@users.noreply.github.com>
2024-08-10 18:08:28 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score1 = 33;
+let score2 = "33abc"
+
+console.log(typeof score1);
+console.log(typeof(score1));
+
+let valueInNumber = Number(score2)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false =>0
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
