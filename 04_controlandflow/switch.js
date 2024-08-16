dbfbb57769f58c8c4733635f90baabd31d23bb77
ved-mat-pro <131252778+ved-mat-pro@users.noreply.github.com>
2024-08-16 19:07:42 +0000

    #

---
+++ 04_controlandflow/switch.js
@@ -0,0 +1,55 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:                         // use shift + alt + downward arrow to duplicate content easily
+        console.log("january");
+        break;                      // if break is not written then whole code after true statement will execute except default value
+    case 2:
+        console.log("feburary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+
+    default:
+        console.log("default case match ");
+        
+        break;
+}
+
+
+const Month = "march"
+
+switch (Month) {
+    case "jan":                        
+        console.log("january");
+        break;                     
+    case "feb":
+        console.log("feburary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+
+    default:
+        console.log("default case match ");
+        
+        break;
+}
